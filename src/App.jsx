b9a484e6d19fc23e7458daeee2c04ad301d41382
Hernan <hernifleitas235@gmail.com>
2026-02-20 17:50:28 -0300

    Initial Vite React-project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+import JobList from "./components/JobList.jsx";
+
+function App(){
+  return(
+    <div>
+      <h1>Job Aplications</h1>
+      <JobList />
+    </div>
+  );
+
+}
+
+
+export default App;
+
+
